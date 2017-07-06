d62c016c1f11ed6d2abb603420e56b559bf19985
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-07-06 15:10:38 +0200

    timetable game is ready

--- src/main/java/com/greenfox/zoroo/service/MathService.java
+++ src/main/java/com/greenfox/zoroo/service/MathService.java
@@ -2,6 +2,7 @@ package com.greenfox.zoroo.service;
 
 
 import com.greenfox.zoroo.gameLogic.MathGame;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,13 @@ public class MathService {
   @Autowired
   MathGame mathGame;
 
+  private List<String> possibleAnswers;
+  private int rightAnswerIndex;
 
-  public void timeTableGame(int gameLevel, int amountOfPossibleAnswers){
 
+  public void timeTableGame(int gameLevel, int amountOfPossibleAnswers) {
     mathGame.playTimeTableGame(gameLevel, amountOfPossibleAnswers);
-
+    possibleAnswers = mathGame.getPossibleAnswers();
+    rightAnswerIndex = mathGame.getIndexOfTheRightAnswerInThePossibleAnswers();
   }
-
 }
