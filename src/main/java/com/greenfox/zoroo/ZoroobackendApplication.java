85ad0266c456accbb6b85107318616ddbdeb121f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-06 10:06:55 +0200

    add initial project files

---
+++ src/main/java/com/greenfox/zoroo/ZoroobackendApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.zoroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoroobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoroobackendApplication.class, args);
+	}
+}
