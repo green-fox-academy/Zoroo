0f27b6bff18159777759dd52b1e201ccea379554
Ramin <RaminShamsi@yahoo.com>
2017-07-06 17:24:10 +0200

    add the loging from Database

---
+++ src/main/java/com/greenfox/zoroo/Repository/UserRepo.java
@@ -0,0 +1,15 @@
+package com.greenfox.zoroo.Repository;
+
+import com.greenfox.zoroo.model.UserProfile;
+import com.greenfox.zoroo.model.dto.LoginFormDto;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<UserProfile, Long> {
+
+  List<UserProfile> findAll();
+  boolean existsByUsername(String username);
+  boolean existsByPassword(String password);
+}
