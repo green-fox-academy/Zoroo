04ad7ee80b1a30ebbc9754028546d4a2babe5539
Ramin <RaminShamsi@yahoo.com>
2017-07-07 16:54:04 +0200

    cleaned the home controller

--- src/main/java/com/greenfox/zoroo/controller/HomeController.java
+++ src/main/java/com/greenfox/zoroo/controller/HomeController.java
@@ -1,22 +1,14 @@
 package com.greenfox.zoroo.controller;
 
-import com.greenfox.zoroo.gameLogic.GeographicInfoGame;
-import com.greenfox.zoroo.model.Game;
-import com.greenfox.zoroo.model.GameType;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  @Autowired
-  GeographicInfoGame geographicInfoGame;
+
   @GetMapping(value = "/")
   public String getIndexPage() {
-
-    Game game = new Game();
-    game.setGameType("GEOGRAPHY");
-    geographicInfoGame.playGeographicGame(game);
     return "index";
   }
 }
