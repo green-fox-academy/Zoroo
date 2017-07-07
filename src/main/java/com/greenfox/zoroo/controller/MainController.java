6aebccfb525108e531fb9495481ec69908e299bf
Garlyle <kovacsmilan85@gmail.com>
2017-07-07 10:04:43 +0200

    create statistics html

--- src/main/java/com/greenfox/zoroo/controller/MainController.java
+++ src/main/java/com/greenfox/zoroo/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.greenfox.zoroo.controller;
 
+import com.greenfox.zoroo.model.UserProfile;
 import com.greenfox.zoroo.model.dto.GameStartDto;
 import com.greenfox.zoroo.model.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
@@ -15,4 +18,11 @@ public class MainController {
     model.addAttribute("game", new GameStartDto());
     return "main";
   }
+
+  @GetMapping(value ="/statistics")
+  public String getStatistics(Model model) {
+    model.addAttribute("user", new UserDto("Jancsi"));
+    return "statistics";
+  }
+
 }
