578817ce57a06710e0ab8b7e00fded51adfd5ecf
Garlyle <kovacsmilan85@gmail.com>
2017-07-07 10:54:40 +0200

    rework navbar

--- src/main/java/com/greenfox/zoroo/controller/MainController.java
+++ src/main/java/com/greenfox/zoroo/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.greenfox.zoroo.controller;
 
-import com.greenfox.zoroo.model.UserProfile;
+import com.greenfox.zoroo.model.NavLink;
 import com.greenfox.zoroo.model.dto.GameStartDto;
 import com.greenfox.zoroo.model.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 
 @Controller
 public class MainController {
+
+  @ModelAttribute
+  public void addAttributes(Model model) {
+    ArrayList<NavLink> links = new ArrayList<>();
+    links.add(new NavLink("/main", "Játék"));
+    links.add(new NavLink("/statistics", "Statisztika"));
+    links.add(new NavLink("/logout", "Kijelentkezés"));
+    model.addAttribute("nav", links);
+  }
 
   @GetMapping(value = {"/main", "/main/"})
   public String getMainPage(Model model) {
